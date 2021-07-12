7268e4c58d8764479fa4440cde37d3054995ad72
YunYouJun <me@yunyoujun.cn>
2021-07-12 23:26:29 +0800

    :bug: fix build:example by common config

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,57 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfigExport } from "vite";
 import Vue from "@vitejs/plugin-vue";
-import Icons, { ViteIconsResolver } from 'vite-plugin-icons'
-import Components from 'vite-plugin-components'
+import Icons, { ViteIconsResolver } from "vite-plugin-icons";
+import Components from "vite-plugin-components";
 import path from "path";
 import pkg from "./package.json";
+
+const commonConfig: UserConfigExport = {
+  resolve: {
+    alias: {
+      "~/": `${path.resolve(__dirname, "src")}/`,
+    },
+  },
+};
 
 // https://vitejs.dev/config/
 export default ({ command, mode }) => {
   if (mode === "example") {
-    return defineConfig({
-      base: `/${pkg.name}/`,
-      plugins: [Vue()],
-    });
+    return defineConfig(
+      Object.assign(commonConfig, {
+        base: `/${pkg.name}/`,
+        plugins: [Vue()],
+      })
+    );
   } else {
-    return defineConfig({
-      resolve: {
-        alias: {
-          '~/': `${path.resolve(__dirname, 'src')}/`,
-        },
-      },
-      plugins: [
-        Vue(),
-        Icons(),
-        Components({
-        customComponentResolvers: ViteIconsResolver(),
-      })],
-      build: {
-        cssCodeSplit: false,
-        lib: {
-          entry: path.resolve(__dirname, "src/index.ts"),
-          name: "VueAboutMe",
-        },
-        rollupOptions: {
-          // make sure to externalize deps that shouldn't be bundled
-          // into your library
-          external: ["vue"],
-          output: {
-            // Provide global variables to use in the UMD build
-            // for externalized deps
-            globals: {
-              vue: "Vue",
+    return defineConfig(
+      Object.assign(commonConfig, {
+        plugins: [
+          Vue(),
+          Icons(),
+          Components({
+            customComponentResolvers: ViteIconsResolver(),
+          }),
+        ],
+        build: {
+          cssCodeSplit: false,
+          lib: {
+            entry: path.resolve(__dirname, "src/index.ts"),
+            name: "VueAboutMe",
+          },
+          rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: ["vue"],
+            output: {
+              // Provide global variables to use in the UMD build
+              // for externalized deps
+              globals: {
+                vue: "Vue",
+              },
             },
           },
         },
-      },
-    });
+      })
+    );
   }
 };
